06ccfd6c2ad6780c75393f18a9fa425d2e64670a
Easlee Liu <easlee@outlook.com>
2022-10-08 12:13:00 +0800

    regenerated

---
+++ vs2022/fmp-xtc-drugface-lib-mvcs/Permissions.cs
@@ -0,0 +1,13 @@
+
+namespace XTC.FMP.MOD.DrugFace.LIB.MVCS
+{
+    public class Permissions
+    {
+
+        public const string HealthyCreate = "/XTC/DrugFace/Healthy/Create";
+        public const string HealthyUpdate = "/XTC/DrugFace/Healthy/Update";
+        public const string HealthyRetrieve = "/XTC/DrugFace/Healthy/Retrieve";
+        public const string HealthyDelete = "/XTC/DrugFace/Healthy/Delete";
+
+    }
+}
